5cdbeed3152d7cb456ac57374af585d9074a15cb
Freddie <freddiejmo@gmail.com>
2019-04-24 15:50:00 -0400

    if conditional

---
+++ 10-conditionals/1-if.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+age = 18
+m_age = 18
+
+if age >= m_age:
+    print "You're of age"
+    if True:
+        "this is executed as long as he is of age"
+
+age = 52
+if age >= m_age:
+    print "You're of age"
+    if True:
+        print "this is executed as long as he is of age"
