594a66e939d7de688df0420214f1c67ee71e731f
Freddie <freddiejmo@gmail.com>
2019-04-23 21:16:26 -0400

    strings scaping

---
+++ 4-strings-type/2-scaping.py
@@ -0,0 +1,15 @@
+# scaping strings
+s = 'scaping with \n line'
+s2 = "scaping with \n\t line and tab"
+print s
+print s2
+s3 = """scaping multiple lines, line 1
+line 2
+line 3
+lin4
+"""
+print s3
+
+
+
+
