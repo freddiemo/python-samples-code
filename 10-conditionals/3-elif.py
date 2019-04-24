e010a1a2845e773f7f7f7a437b37927a5f4617fd
Freddie <freddiejmo@gmail.com>
2019-04-24 15:50:28 -0400

    elif

---
+++ 10-conditionals/3-elif.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+age = 18
+m_age = 18
+
+if age >= 0 and age < 18:
+    print 'You are a child'
+elif age >= 18 and age < 27:
+    print 'You are a young'
+elif age >= 27 and age < 60:
+    print "You are an adult"
+else:
+    print "You are a seniors"
